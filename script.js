480b63f4a1041d12835b7684c6f8c6143811fcec
harrisonblackburn <blackbhr@yahoo.com>
2021-06-05 14:25:49 -0500

    able to retrieve current weather data for cities

---
+++ script.js
@@ -0,0 +1,33 @@
+var weatherDetailsTableEl = document.getElementById('weatherDetailsTable')
+// var forecastDetailsEl = document.getElementById('forecastDetails');
+
+function getApi(city) {
+    let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&APPID=8b50af280b34989ae4a92a4a03f30c01';
+    fetch(requestUrl)
+    .then (function (res){
+        return res.json();
+    })
+    .then(function (data){
+        console.log(data);
+    
+
+
+    var tableRow = document.createElement('tr'); 
+
+         tableRow.innerHTML = 
+      
+        `<td>  ${data.main.temp} F</td>
+         <td> ${data.main.humidity} % </td>
+         <td> ${data.wind.speed} MPH </td>`;
+
+      weatherDetailsTable.append(tableRow);
+      })
+}
+
+$(document).on('click', '#userInputCityButton', function (){
+    var userInputCityEl = document.getElementById('userInputCity').value;
+   
+    getApi(userInputCityEl)
+}); 
+
+
